2c86e993e37004306dfed2e065b152956ddc5fdd
Manenomyk <manenomichael99@gmail.com>
2024-06-22 11:34:04 +0300

    students factory

--- database/factories/studentsFactory.php
+++ database/factories/studentsFactory.php
@@ -17,7 +17,8 @@ class studentsFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail(),
         ];
     }
 }
