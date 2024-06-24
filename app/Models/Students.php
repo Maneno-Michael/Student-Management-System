06c86cb8e2c6825f46c3a701beb48751463c42c6
Manenomyk <manenomichael74@gmail.com>
2024-06-24 15:22:06 +0300

    alignmenta

--- app/Models/Students.php
+++ app/Models/Students.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Students extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'email', 'classes_id', 'sections_id'];
+    protected $fillable = [
+        'name',
+        'email',
+        'classes_id',
+        'sections_id'
+    ];
 
     public function class()
     {
@@ -19,6 +24,3 @@ class Students extends Model
         return $this->belongsTo(Sections::class);
     }
 }
-
-
- 
